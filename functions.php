054962f3623112850cb926e89fd3ad29b0df0477
andornagym <andornagy@outlook.com>
2022-09-10 17:45:54 +0300

    Initail setup

---
+++ functions.php
@@ -0,0 +1,5 @@
+<?php
+
+include "functions/enquque.php";
+include "functions/query.php";
+include "functions/theme-functions.php";
--- index.php
+++ index.php
@@ -1,1 +1,5 @@
-s
+<?php get_header(); ?>
+
+Test
+
+<?php get_footer(); ?>
